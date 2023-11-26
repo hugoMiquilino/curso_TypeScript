fc4b7326b3c874764a060c1b3cfec7491f4370dc
Hugo Miquilino <hugomiquilino@gmail.com>
2023-11-26 20:18:29 -0300

    Seção 01 - Introdução ao TS

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+function component() {
+    const element = document.getElementById('content');
+
+    element.innerHTML = "<h1>Hello World</h1>"
+    element.innerHTML += "<h1>Hello World</h1>"
+    element.innerHTML += "<h1>Hello World</h1>"
+    element.innerHTML += "<h1>Hello World</h1>"
+    element.innerHTML += "<h1>Hello World</h1>"
+    element.innerHTML += "<h1>Hello World</h1>"
+    element.innerHTML += "<h1>Hello World</h1>"
+
+    return element;
+  }
+
+  component();
